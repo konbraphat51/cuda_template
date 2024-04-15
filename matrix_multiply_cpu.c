0a1a5f70db9e4e5435c6850aa388ec1c4a182f57
konbraphat51 <konbraphat@gmail.com>
2024-04-15 14:12:10 +0900

    cpu computation

--- matrix_multiply_cpu.c
+++ matrix_multiply_cpu.c
@@ -18,8 +18,39 @@ int **generate_matrix() {
     return matrix;
 }
 
+int **multiply_matrices_3for(int **matrix_input0, int **matrix_input1) {
+    int i, j, k;
+    int **matrix_output = (int **)malloc(SIZE * sizeof(int *));
+
+    for (i = 0; i < SIZE; i++) {
+        matrix_output[i] = (int *)malloc(SIZE * sizeof(int));
+        for (j = 0; j < SIZE; j++) {
+            matrix_output[i][j] = 0;
+            for (k = 0; k < SIZE; k++) {
+                matrix_output[i][j] += matrix_input0[i][k] * matrix_input1[k][j];
+            }
+        }
+    }
+
+    return matrix_output;
+}
+
 int main() {
-    int 
+    int i, j, k;
+    int **matrix_input0 = generate_matrix();
+    int **matrix_input1 = generate_matrix();
+
+    //Start time
+    time_t time_start = time(NULL);
+
+    //compute (CPU)
+    int **matrix_output_cpu = multiply_matrices_3for(matrix_input0, matrix_input1);
+
+    //End time
+    time_t time_end = time(NULL);
+
+    //Print the execution time
+    printf("Execution(CPU) time: %ld seconds\n", time_end - time_start);
 
     return 0;
 }
